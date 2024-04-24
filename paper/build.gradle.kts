b21fddd279617124133192047a28177d02599f52
Ivan Filipenkov <hello@vanutp.dev>
2024-04-24 23:33:52 +0200

    move paper to kotlin gradle dsl

---
+++ paper/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("io.papermc.paperweight.userdev")
+    id("xyz.jpenilla.run-paper")
+}
+
+repositories {
+    maven {
+        name = "papermc-repo"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+    maven {
+        name = "sonatype"
+        url = uri("https://oss.sonatype.org/content/groups/public/")
+    }
+}
+
+dependencies {
+    paperweight.paperDevBundle("1.20.1-R0.1-SNAPSHOT")
+    implementation(project(":common"))
+}
+
+
+tasks {
+    processResources {
+        inputs.property("version", project.version)
+
+        filesMatching("plugin.yml") {
+            expand("version" to project.version)
+        }
+    }
+
+    reobfJar {
+        outputJar.set(rootProject.layout.buildDirectory.file("release/${rootProject.name}-${rootProject.version}-${project.name}.jar"))
+    }
+
+    assemble {
+        dependsOn(reobfJar)
+    }
+}
+
