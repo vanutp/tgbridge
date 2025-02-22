9347950307dc1aabdf7f9296a065efcd9851208c
RedstoneFX <zatmonhp@yandex.ru>
2025-02-22 17:15:00 +0300

    PAPER: /tghide and /tgshow

--- paper/src/main/kotlin/dev/vanutp/tgbridge/paper/EventManager.kt
+++ paper/src/main/kotlin/dev/vanutp/tgbridge/paper/EventManager.kt
@@ -88,16 +88,26 @@ class EventManager(private val plugin: PaperBootstrap) : Listener {
 
     private fun registerCommandHandlers() {
         plugin.getCommand("tgbridge")!!.setExecutor { commandSender, _, _, args ->
-            if (args.toList() != listOf("reload")) {
-                return@setExecutor false
-            }
-            return@setExecutor plugin.tgbridge.onReloadCommand(
-                TBCommandContext(
-                    reply = { text ->
-                        commandSender.sendMessage(text)
-                    }
+            if (args[0] == "reload") {
+                return@setExecutor plugin.tgbridge.onReloadCommand(
+                    TBCommandContext(
+                        reply = { text ->
+                            commandSender.sendMessage(text)
+                        }
+                    )
                 )
-            )
+            }
+            return@setExecutor false
+        }
+
+        plugin.getCommand("tgshow")!!.setExecutor { commandSender, _, _, args ->
+            commandSender.getServer().getPlayer(commandSender.getName())?.removeScoreboardTag("hidden-telegram")
+            return@setExecutor true
+        }
+
+        plugin.getCommand("tghide")!!.setExecutor { commandSender, _, _, args ->
+            commandSender.getServer().getPlayer(commandSender.getName())?.addScoreboardTag("hidden-telegram")
+            return@setExecutor true
         }
     }
 }
--- paper/src/main/kotlin/dev/vanutp/tgbridge/paper/PaperPlatform.kt
+++ paper/src/main/kotlin/dev/vanutp/tgbridge/paper/PaperPlatform.kt
@@ -5,13 +5,18 @@ import net.kyori.adventure.text.Component
 import net.minecraft.locale.Language
 import org.bukkit.plugin.java.JavaPlugin
 import kotlin.io.path.absolute
+import org.bukkit.Bukkit
 
 class PaperPlatform(private val plugin: JavaPlugin) : Platform() {
     override val name = "paper"
     override val configDir = plugin.dataFolder.toPath().absolute()
 
     override fun broadcastMessage(text: Component) {
-        plugin.server.broadcast(text)
+        for (p in Bukkit.getOnlinePlayers()) {
+            if(!p.getScoreboardTags().contains("hidden-telegram")) {
+                p.sendMessage(text)
+            }
+        }
     }
 
     override fun getOnlinePlayerNames(): Array<String> {
