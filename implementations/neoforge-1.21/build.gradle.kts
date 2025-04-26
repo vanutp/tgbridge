b18842602031efc14359c06a19ae634309107634
Ivan Filipenkov <hello@vanutp.dev>
2025-04-26 09:50:03 +0200

    Add 1.21.5 to supported versions on modrinth

--- implementations/neoforge-1.21/build.gradle.kts
+++ implementations/neoforge-1.21/build.gradle.kts
@@ -70,7 +70,7 @@ tasks {
 
 modrinth {
     uploadFile.set(tasks.remapJar)
-    gameVersions.addAll("1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4")
+    gameVersions.addAll("1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4", "1.21.5")
     loaders.addAll("neoforge")
     dependencies {
         required.project("kotlin-for-forge")
