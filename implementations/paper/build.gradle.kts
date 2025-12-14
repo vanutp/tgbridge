36b75dc93e7d0ed8fa608228e1721a71d9cbc468
Ivan Filipenkov <hello@vanutp.dev>
2025-12-14 01:55:36 +0100

    fix paper modrinth upload

--- implementations/paper/build.gradle.kts
+++ implementations/paper/build.gradle.kts
@@ -105,7 +105,7 @@ kotlin {
 }
 
 modrinth {
-    uploadFile.set(tasks.reobfJar.get().outputJar)
+    uploadFile.set(tasks.shadowJar)
     gameVersions.addAll(
         "1.19", "1.19.1", "1.19.2", "1.19.3", "1.19.4",
         "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4",
