a7a40adf8b03fd17dc73ac71f4293bde4491e42f
Ivan Filipenkov <fox@vanutp.dev>
2026-02-05 20:35:49 +0100

    fix crash on 26.1 snapshots

--- implementations/fabric/src/main/java/dev/vanutp/tgbridge/fabric/TgbridgeMixinPlugin.java
+++ implementations/fabric/src/main/java/dev/vanutp/tgbridge/fabric/TgbridgeMixinPlugin.java
@@ -4,6 +4,8 @@ import com.google.common.collect.ImmutableMap;
 import com.google.gson.JsonParseException;
 import net.minecraft.DetectedVersion;
 import net.minecraft.util.GsonHelper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
@@ -15,21 +17,29 @@ import java.util.Map;
 import java.util.Set;
 
 public final class TgbridgeMixinPlugin implements IMixinConfigPlugin {
+    private static final Logger logger = LogManager.getLogger("tgbridge-mixin");
     private static final String version;
     private static final int major;
     private static final int minor;
     private static final int patch;
 
     static {
+        final String fullVersion;
         try (final var is = DetectedVersion.class.getResourceAsStream("/version.json")) {
             if (is == null) {
                 throw new IllegalStateException("Failed to load Minecraft version");
             }
             try (final var reader = new InputStreamReader(is)) {
-                version = GsonHelper.getAsString(GsonHelper.parse(reader), "name");
+                fullVersion = GsonHelper.getAsString(GsonHelper.parse(reader), "name");
             }
         } catch (JsonParseException | IOException exception) {
             throw new IllegalStateException("Failed to load Minecraft version", exception);
+        }
+
+        final var fullVersionSplit = fullVersion.split(" ");
+        version = fullVersionSplit[0];
+        if (fullVersionSplit.length > 1) {
+            logger.warn("[tgbridge] Minecraft snapshots aren't well supported, things may break!");
         }
 
         final var splitVersion = version.split("\\.");
