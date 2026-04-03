ceaae82274bbb9a1a3f410b02bed31c2ab75f8fc
Ivan Filipenkov <fox@vanutp.dev>
2026-04-03 06:50:48 +0200

    add jdk to devshell

--- shell.nix
+++ shell.nix
@@ -3,8 +3,11 @@ pkgs.mkShell {
   packages = [
     (pkgs.writers.writeNuBin "setup-mc" {
       makeWrapperArgs = [
-        "--set" "TGBRIDGE_ROOT" (builtins.toString ./.)
+        "--set"
+        "TGBRIDGE_ROOT"
+        (builtins.toString ./.)
       ];
     } (builtins.readFile ./setup-mc.nu))
   ];
+  JAVA_HOME = pkgs.temurin-bin-25;
 }
